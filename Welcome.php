16c7f20257c45cdeffb02c0532e73e6e4ae26eba
plumteach <49849159+plumteach@users.noreply.github.com>
2019-05-08 18:15:08 +1200

    githun.io

---
+++ Welcome.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Answer</title>
+</head>
+
+<body>
+Welcome <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?>
+</body>
+</html>
